53acd09d75448d92381c343bd772bb47464a9307
sidney JR <44652599+sidjrr@users.noreply.github.com>
2018-11-03 10:46:47 -0300

    modoficação

--- main.c
+++ main.c
@@ -1,33 +1,70 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include "registro.h"
-#include "Funcoes.h"
-
+#include "registro.h" // chamando as informação que esta em registro.h  que e a nossa tad 
 
 int main(int argc, char const *argv[]) {
 
-    int N; // N = tamanho do vetor a ser ordenado
+
+    int N,j,k; // N = tamanho do vetor a ser ordenado
+    printf("-->");
     scanf("%d",&N);
     //Registro reg[N]; = alocaçao estatica
-    // alocação dinamica dos registros
-
+    //alocação dinamica;
+    printf("Preencher vetor\n");
     Registro *reg = (Registro *) malloc(sizeof(Registro) * N);
-    Registro_simples *reg_simples = (Registro_simples *) malloc(sizeof(Registro) * N);
-
-    printf("Preencher registros\n");
     for (int i = 0; i < N; i++) {
       reg[i].chave = rand() % 100;
-      printf("reg %d\n",reg[i].chave);
-    }
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < 10; j++) {
-        reg[i].cadeia[j][0] = 'a';
-        printf("reg2 %c\n",reg[i].cadeia[j][0]);
-      }
+      printf("chave: %d\n",reg[i].chave);
+        for(j=0;j<4;j++){
+            reg[i].v[j]=(rand()%100)/1.5;
+        }
+        printf("vetor V: %.2f\t",reg[i].v[j]);
+        
+        for(j=0;j<3;j++){
+            for(k=0;k<10;k++){
+                reg[i].cadeia[j][k] ='A'+rand()%26;
+                printf("CADEIA DE STRING: %d",reg[i].cadeia[j][k]);
+            }
+        } 
+    
+    
     }
 
-
+//   //  //tentando alocar matriz e preencher ela 
+//   //   Registro **regi = (Registro **)malloc(j * sizeof(Registro *));
+//   //   for (j=0; j<10; j++){
+//   //     regi[j] = (Registro *) malloc(k * sizeof(Registro));
+//   //     for(k=0; k<200; k++){
+//   //       regi[k]->cadeia = rand() % 100;
+//   //       printf("%c%c\n",regi[j][k].cadeia);
+//   //     }
+//   //   }
+//  */
+//     int i,j,k;
+//     Registro *reg=(Registro *) malloc(N* sizeof(Registro));
+//     for(i=0;i<N;i++){
+//         reg[i].chave=rand()%1000;
+//         for(j=0;j<4;j++){
+//             reg[i].v[j]=(rand()%100)/1.5;
+//         }
+//         for(j=0;j<3;j++){
+//             for(k=0;k<10;k++){
+//                 reg[i].cadeia[j][k] ='A'+rand()%26;
+//             }
+//         }
+//     }
+//     printf("VETOR NAO ORDENADO:\n");
+//     for(i=0;i<N;i++){
+//         printf("chave: %d\n",reg[i].chave);
+//         // for(j=0;j<4;j++){
+//         //     printf("vetor V: %.2f\t",reg[i].v[j]);
+//         // }
+//         // for(j=0;j<3;j++){
+//         //     for(k=0;k<10;k++){
+//         //         printf("CADEIA DE STRING: %d",reg[i].cadeia[j][k]);
+//         //     }
+//         // }
+//     }
+  printf("%d\n",reg);
+  free(reg);
+  printf("%d\n",reg); 
   return 0;
 }
