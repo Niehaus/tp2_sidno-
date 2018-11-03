8dbeda6b490ec5f17adf53aae903fdebd878a844
sidney JR <44652599+sidjrr@users.noreply.github.com>
2018-11-03 11:24:23 -0300

    Update main.c
    limpando codigo

--- main.c
+++ main.c
@@ -11,64 +11,26 @@ int main(int argc, char const *argv[]) {
     printf("Preencher vetor\n");
     Registro *reg = (Registro *) malloc(sizeof(Registro) * N);
     for (int i = 0; i < N; i++) {
-      printf("chave %d:\n -->",i);
+      printf("chave %d:\n",i);
       reg[i].chave = rand() % 100;
       printf("%d\n",reg[i].chave);
        
-      printf("vetor %d:\n -->",j);
+      printf("vetor %d:\n",j);
       for(j=0;j<4;j++){
         reg[i].v[j]=(rand()%100)/1.5;        
         printf("%.2f\t",reg[i].v[j]);
         }
         printf("\n");
-        printf("cadeia %d:\n -->\n ",i);
+        printf("cadeia %d:\n",i);
         for(j=0;j<3;j++){    
-            for(k=0;k<10;k++){
+            for(k=0;k<200;k++){
                 reg[i].cadeia[j][k] ='A'+rand()%26;
-                printf("%d\t",reg[i].cadeia[j][k]);
+            printf("%c\t",reg[i].cadeia[j][k]);
             }
-            printf("\n");
         } 
         printf("\n");    
     }
-
-//   //  //tentando alocar matriz e preencher ela 
-//   //   Registro **regi = (Registro **)malloc(j * sizeof(Registro *));
-//   //   for (j=0; j<10; j++){
-//   //     regi[j] = (Registro *) malloc(k * sizeof(Registro));
-//   //     for(k=0; k<200; k++){
-//   //       regi[k]->cadeia = rand() % 100;
-//   //       printf("%c%c\n",regi[j][k].cadeia);
-//   //     }
-//   //   }
-//  */
-//     int i,j,k;
-//     Registro *reg=(Registro *) malloc(N* sizeof(Registro));
-//     for(i=0;i<N;i++){
-//         reg[i].chave=rand()%1000;
-//         for(j=0;j<4;j++){
-//             reg[i].v[j]=(rand()%100)/1.5;
-//         }
-//         for(j=0;j<3;j++){
-//             for(k=0;k<10;k++){
-//                 reg[i].cadeia[j][k] ='A'+rand()%26;
-//             }
-//         }
-//     }
-//     printf("VETOR NAO ORDENADO:\n");
-//     for(i=0;i<N;i++){
-//         printf("chave: %d\n",reg[i].chave);
-//         // for(j=0;j<4;j++){
-//         //     printf("vetor V: %.2f\t",reg[i].v[j]);
-//         // }
-//         // for(j=0;j<3;j++){
-//         //     for(k=0;k<10;k++){
-//         //         printf("CADEIA DE STRING: %d",reg[i].cadeia[j][k]);
-//         //     }
-//         // }
-//     }
-  printf("%d\n",reg);
+  printf("ponteiro reg",reg);
   free(reg);
-  printf("%d\n",reg); 
   return 0;
 }
