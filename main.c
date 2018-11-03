233154d92dc68b8dbd092083107c25110badc1a3
sidney JR <44652599+sidjrr@users.noreply.github.com>
2018-11-03 11:03:36 -0300

    Add files via upload
    todos os vetores estão preenchidos

--- main.c
+++ main.c
@@ -11,21 +11,25 @@ int main(int argc, char const *argv[]) {
     printf("Preencher vetor\n");
     Registro *reg = (Registro *) malloc(sizeof(Registro) * N);
     for (int i = 0; i < N; i++) {
+      printf("chave %d:\n -->",i);
       reg[i].chave = rand() % 100;
-      printf("chave: %d\n",reg[i].chave);
-        for(j=0;j<4;j++){
-            reg[i].v[j]=(rand()%100)/1.5;
+      printf("%d\n",reg[i].chave);
+       
+      printf("vetor %d:\n -->",j);
+      for(j=0;j<4;j++){
+        reg[i].v[j]=(rand()%100)/1.5;        
+        printf("%.2f\t",reg[i].v[j]);
         }
-        printf("vetor V: %.2f\t",reg[i].v[j]);
-        
-        for(j=0;j<3;j++){
+        printf("\n");
+        printf("cadeia %d:\n -->\n ",i);
+        for(j=0;j<3;j++){    
             for(k=0;k<10;k++){
                 reg[i].cadeia[j][k] ='A'+rand()%26;
-                printf("CADEIA DE STRING: %d",reg[i].cadeia[j][k]);
+                printf("%d\t",reg[i].cadeia[j][k]);
             }
+            printf("\n");
         } 
-    
-    
+        printf("\n");    
     }
 
 //   //  //tentando alocar matriz e preencher ela 
