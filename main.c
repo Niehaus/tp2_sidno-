3d47ad4ace5d00aad363096a69b3b9cbedd68763
sidney JR <44652599+sidjrr@users.noreply.github.com>
2018-11-04 23:12:54 -0200

    Add files via upload
    agora com arquivo

--- main.c
+++ main.c
@@ -3,11 +3,19 @@
 int main(int argc, char const *argv[]) {
 int i,j,x,y,z; //vareaveis
 //----------------------------------------------------------------------------------------------------------
+//ARQUIVO
+FILE *arq;
 
-//preenche os regiostro: 
+arq=fopen("entrada.txt","w");
+if(arq== NULL){
+  printf("ERRO AO ABRIR O ARQUIVO");
+  return 1;
+}
+
+//preenche os vetores: 
   
-    int N,j,k; // N = tamanho do vetor a ser ordenado
-    printf("-->");
+    int N,k; // N = tamanho do vetor a ser ordenado
+    printf("DIGITE QUANTAS VEZES DESEJA PREENCHER O VETOR:\n-->");
     scanf("%d",&N);
     //Registro reg[N]; = alocaçao estatica
     //alocação dinamica;
@@ -15,32 +23,40 @@ int i,j,x,y,z; //vareaveis
     Registro *reg = (Registro *) malloc(sizeof(Registro) * N);
     for (int i = 0; i < N; i++) {
       printf("chave %d:\n",i);
+      fprintf(arq,"chave %d:\n",i);
       reg[i].chave = rand() % 100;
+      fprintf(arq,"%d\n",reg[i].chave);
       printf("%d\n",reg[i].chave);
        
       printf("vetor %d:\n",j);
+      fprintf(arq,"Vetor %d:\n",j);
       for(j=0;j<4;j++){
-        reg[i].v[j]=(rand()%100)/1.5;        
+        reg[i].v[j]=(rand()%100)/1.5;
+        fprintf(arq,"%.2f\t",reg[i].v[j]);        
         printf("%.2f\t",reg[i].v[j]);
         }
         printf("\n");
         printf("cadeia %d:\n",i);
+        fprintf(arq,"cadeia %d:\n",i);
         for(j=0;j<3;j++){    
             for(k=0;k<200;k++){
                 reg[i].cadeia[j][k] ='A'+rand()%26;
-            printf("%c\t",reg[i].cadeia[j][k]);
+                fprintf(arq,"%c\t",reg[i].cadeia[j][k]);
+                printf("%c\t",reg[i].cadeia[j][k]);
             }
         } 
         printf("\n");    
     }
+  fclose(arq);
   printf("%d\n",reg);
   free(reg);
   
 //---------------------------------------------------------------------------------------------------
   //loop para a função quick
-  while(x=0, x <=5; x++){
+  /*while(x=0, x <=5, x++){
       //chama função quick
       //armazena a media no arquivo 
-    }
+    }*/
   return 0;
 }
+
